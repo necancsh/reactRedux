acd99a3edcc027ad92f346150161a9218da2b99b
Craig Harris <cshseattle@gmail.com>
2016-12-11 22:27:39 -0800

    sixth commit

--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -5,6 +5,7 @@ import classnames from 'classnames';
 import validateInput from '../../../server/shared/validations/signup';
 import TextFieldGroup from '../common/TextFieldGroup';
 
+
 class SignupForm extends React.Component {
   constructor(props) {
     super(props);
@@ -42,7 +43,13 @@ class SignupForm extends React.Component {
     if (true) {
       this.setState({ errors: {}, isLoading: true });
       this.props.userSignupRequest(this.state).then(
-        () => {},
+        () => {
+          this.props.addFlashMessage({
+            type: 'success',
+            text: 'You signed up successfully. Welcome!'
+          });
+          this.context.router.push('/');
+        },
         ({ data }) => this.setState({ errors: data, isLoading: false })
       );
     }
@@ -111,7 +118,12 @@ class SignupForm extends React.Component {
 }
 
 SignupForm.propTypes = {
-  userSignupRequest: React.PropTypes.func.isRequired
+  userSignupRequest: React.PropTypes.func.isRequired,
+  addFlashMessage: React.PropTypes.func.isRequired
+}
+
+SignupForm.contextTypes = {
+  router: React.PropTypes.object.isRequired
 }
 
 export default SignupForm;

781c351194d637183ac1d43b792b50aa8e0ac0da
Craig Harris <cshseattle@gmail.com>
2016-12-14 17:08:31 -0800

    12th commit

--- client/reducers/auth.js
+++ client/reducers/auth.js
@@ -1,5 +1,6 @@
-import SET_CURRENT_USER from '../actions/types';
+import { SET_CURRENT_USER } from '../actions/types';
 import isEmpty from 'lodash/isEmpty';
+
 const initialState = {
   isAuthenticated: false,
   user: {}
